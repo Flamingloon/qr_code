1cd828ed716c2179b4fad2bbea870675bad307c1
Sahil Totala <81063988+Flamingloon@users.noreply.github.com>
2022-01-05 18:49:48 +0530

    added firebase core dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_code/scanner.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
+
 
   // This widget is the root of your application.
   @override
@@ -30,16 +36,41 @@ class MyHomePage extends StatefulWidget {
 
   final String title;
 
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var user;
 
-  GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['email']);
+
+  Future<UserCredential> signInWithGoogle() async {
+    await Firebase.initializeApp();
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+    user = googleUser;
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+
+  // GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['email']);
+
+
+
   @override
   Widget build(BuildContext context) {
-    GoogleSignInAccount? user = _googleSignIn.currentUser;
+    // GoogleSignInAccount? user = _googleSignIn.currentUser;
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -48,7 +79,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: ElevatedButton(
             child: user!=null? Text("Scan"): Text("Login") ,
             onPressed: () async {
-              await _googleSignIn.signIn();
+              await signInWithGoogle();
               setState(() {
 
 
